36be4fa0a8f874b2e57d84633b01183dbe9a3fa8
diwei00 <2945608334@qq.com>
2022-10-24 17:22:13 +0800

    反射机制学习

---
+++ Student.java
@@ -0,0 +1,41 @@
+package demo3;
+
+public class Student {
+    private int age = 20;
+    private String name = "ww";
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    private void function(String name) {
+        System.out.println(name);
+    }
+    public Student() {
+
+    }
+
+    private Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
